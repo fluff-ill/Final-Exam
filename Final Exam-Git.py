fe2eb0c07f531b6840af6fae1caf2a6a455dc5e8
fluff-ill <48022366+fluff-ill@users.noreply.github.com>
2019-03-11 13:18:05 -0400

    Fix Sum

--- Final Exam-Git.py
+++ Final Exam-Git.py
@@ -10,17 +10,20 @@ dice = int(input("How many dice do you want to roll? "))
 sides = int(input("How many sides do you want the dice to have? "))
 sum = input("Do you want to add your dice together? (yes or no): ")
 roll = True
-amount = 0
 
 while roll:
+    amount = 0
     print("Rolling dice...")
     for i in range (dice):
         time.sleep(1)
         number = random.randint(1, sides)
         print("You rolled", number)
-        if sum == "yes":
-            amount = amount + number
-    print amount
+        amount = amount + number
+    if sum == "yes":
+        print amount
+    else: 
+        time.sleep(0)
+    
     roll = input("do you want to roll again? (yes or no): ")
     if roll == "yes":
         dice = int(input("How many dice do you want to roll? " ))
