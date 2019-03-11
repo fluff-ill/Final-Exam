640d85c370bacb469376f985b57adeb94ae9ee6a
fluff-ill <48022366+fluff-ill@users.noreply.github.com>
2019-03-11 11:17:47 -0400

    Add amount of Dice

--- Final Exam-Git.py
+++ Final Exam-Git.py
@@ -5,14 +5,16 @@
 
 import random
 
-
+dice = input("How many dice do you  want to roll?")
 roll = True
 
 while roll:
     print("Rolling dice...")
-    print("You rolled", random.randint(1, 6))
+    for i in range (dice):
+        print("You rolled", random.randint(1, 6))
     roll = input("do you want to roll again? (yes or no): ")
     if roll == "yes":
+        dice = int(input("How many dice do you  want to roll?" ))
         continue
     else:
         break
