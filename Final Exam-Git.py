dec7e7f09ae0fccd83733165b4f667aae6abbaf8
fluff-ill <48022366+fluff-ill@users.noreply.github.com>
2019-03-12 16:37:13 -0400

    Add Function

--- Final Exam-Git.py
+++ Final Exam-Git.py
@@ -6,34 +6,37 @@
 import random
 import time
 
-dice = int(input("How many dice do you want to roll? "))
-sides = int(input("How many sides do you want the dice to have? "))
-sum = input("Do you want to add your dice together? (yes or no): ")
-average = input("Do you want to average your dice roll? (yes or no): ")
-roll = True
-
-while roll:
-    amount = 0
-    print("Rolling dice...")
-    for i in range (dice):
-        time.sleep(1)
-        number = random.randint(1, sides)
-        print("You rolled", number)
-        amount = amount + number
-    if sum == "yes":
-        print ("sum: ", amount)
-    if average == "yes":
-        sumav = amount/(float(dice))
-        print ("average: ", sumav)
+def roll_dice(x):
+    dice = int(input("How many dice do you want to roll? "))
+    sides = int(input("How many sides do you want the dice to have? "))
+    sum = input("Do you want to add your dice together? (yes or no): ")
+    average = input("Do you want to average your dice roll? (yes or no): ")
+    roll = True
     
-    roll = input("do you want to roll again? (yes or no): ")
-    if roll == "yes":
-        dice = int(input("How many dice do you want to roll? " ))
-        sides = int(input("How many sides do you want the dice to have? "))
-        sum = input("Do you want to add your dice together? (yes or no): ")
-        average = input("Do you want to average your dice roll? (yes or no): ")
-        continue
-    else:
-        break
+    while roll:
+        amount = 0
+        print("Rolling dice...")
+        for i in range (dice):
+            time.sleep(x)
+            number = random.randint(1, sides)
+            print("You rolled", number)
+            amount = amount + number
+        if sum == "yes":
+            print ("sum: ", amount)
+        if average == "yes":
+            sumav = amount/(float(dice))
+            print ("average: ", sumav)
+        
+        roll = input("do you want to roll again? (yes or no): ")
+        if roll == "yes":
+            dice = int(input("How many dice do you want to roll? " ))
+            sides = int(input("How many sides do you want the dice to have? "))
+            sum = input("Do you want to add your dice together? (yes or no): ")
+            average = input("Do you want to average your dice roll? (yes or no): ")
+            continue
+        else:
+            break
     
-print ("Thank you for rolling dice")
+    print ("Thank you for rolling dice")
+   
+roll_dice(1)
